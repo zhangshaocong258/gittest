ee14516093572affaa025bc397aa493d48658d8f
zhangshaocong258 <597001494@qq.com>
2020-01-27 16:00:09 +0800

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author zhangshaocong
+ * @version 1.0
+ * @Date :Created by 2020-01-27.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("init");
+    }
+}
